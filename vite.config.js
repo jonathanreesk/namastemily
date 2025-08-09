9eb038b86b209560db93cb317a02f6db712204e4
jonathanreesk <jonathanreesk@gmail.com>
2025-08-09 11:11:08 -0500

    Fix Netlify dev server to run functions properly

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: 'public',
-  server: {
-    host: true,
-    port: 8000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8888',
-        changeOrigin: true
-      }
-    }
+    port: 8000
   },
   build: {
     outDir: '../dist'
