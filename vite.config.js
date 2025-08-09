cb88157ab7e8cf457cc46ac3e41d21d8e3bb5bb9
jonathanreesk <jonathanreesk@gmail.com>
2025-08-09 10:54:14 -0500

    Fix Vite server configuration for proper serving

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: 'public',
   server: {
     host: true,
     port: 8000
   },
   build: {
-    outDir: 'dist'
+    outDir: '../dist'
   }
 })
