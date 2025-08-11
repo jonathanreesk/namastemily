0acb60e2a90cf9594eba968192082a98ab9ba178
jonathanreesk <jonathanreesk@gmail.com>
2025-08-11 17:53:33 -0500

    Fix application startup and server configuration

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,9 @@ export default defineConfig({
     open: false,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
-        secure: false,
-        ws: true
+        secure: false
       }
     }
   },
