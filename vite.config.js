0e4091bcb68ccbc45b15ea62a6a6059ea4d027ac
jonathanreesk <jonathanreesk@gmail.com>
2025-08-11 17:51:07 -0500

    Fix Vite proxy configuration

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
+    open: false,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
+        target: 'http://127.0.0.1:3001',
+        changeOrigin: true,
+        secure: false,
+        ws: true
       }
     }
   },
